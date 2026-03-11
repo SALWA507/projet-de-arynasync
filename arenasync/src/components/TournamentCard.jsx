153667574b9a5b7d44cc80fb95c89c05421f4884
SALWA507 <salwaamzane30@gmail.com>
2026-03-11 13:57:50 +0000

    ajouter botton sinscris disinscrit

--- arenasync/src/components/TournamentCard.jsx
+++ arenasync/src/components/TournamentCard.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { useState } from "react";
 import StatusBadge from "./statusBadge";
 
 export default function TournamentCard({ tournament }) {
+  const [isRegistered, setIsRegistered] = useState(false)
   return (
     <div className="bg-white p-4 rounded-xl shadow mt-4">
       <div className="flex justify-between items-center">
@@ -9,6 +10,13 @@ export default function TournamentCard({ tournament }) {
         <StatusBadge status={tournament.status} />
       </div>
       <p className="text-gray-500 text-sm mt-2">{tournament.description}</p>
+      <button
+onClick={()=>setIsRegistered(!isRegistered)}
+className={isRegistered ? "bg-red-500 text-white p-2   rounded-[18px]"  : "bg-green-500 text-white p-2 rounded-[18px]"}
+>
+    {isRegistered ? "Se désinscrire" : "S'inscrire"}
+
+</button>
       <div className="text-sm mt-3 space-y-1 text-gray-600">
         <p>Participants: {tournament.participants.length}</p>
         <p>Type: {tournament.type}</p>
